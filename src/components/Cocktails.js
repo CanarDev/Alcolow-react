f21b5cebbfc869d4563eeeeaf31ecf9cd0ac733a
Soren Messelier-Sentis <sorenmesselier-sentis@mbp-de-soren.home>
2023-01-31 16:02:57 +0100

    :monocle_face: example for calling the API

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Cocktails() {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    const resp = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic');
+    const json = await resp.json();
+    setData(json);
+  }
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-
+  return (
+    <pre>
+      {JSON.stringify(data, null, 2)}
+    </pre>
+  )
 }
-
 export default Cocktails;
