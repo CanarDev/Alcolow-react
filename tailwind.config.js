af6b8ef7c0f3f0a0020ba05ef9f00bd5017220f5
CanarDev <vincent.michel@edu.devinci.Fr>
2023-02-01 14:55:16 +0100

    :lipstick: Add some colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary" : "#007AFF",
+        "secondary" : "#5856D6",
+        "success" : "#34C759",
+        "danger" : "#FF3B30",
+        "warning" : "#FF9500",
+        "grey" : "#F5F5F7",
+        "grey-lighter" : "#FBFBFB",
+        "grey-darker" : "#333333"
+      }
+    },
   },
   plugins: [],
 }
