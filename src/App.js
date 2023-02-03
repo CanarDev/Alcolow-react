4e39cceaee2c791ff9893f2efa2e3de1736cb905
abendafi <bendafischulmann@gmail.com>
2023-02-03 14:44:32 +0100

    firebase login register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Counter from './components/Counter';
-import ToDoList from './components/TodoList';
 import Cocktails from './components/Cocktails';
+import SignUpModal from "./components/SignUpModal";
+import SignInModal from "./components/SignInModal"
+import Private from "./pages/Private/Private"
+import PrivateHome from "./pages/Private/PrivateHome/PrivateHome";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Counter" element={<Counter />} />
-                <Route path="/todolist" element={<ToDoList />} />
-                <Route path="/cocktails" element={<Cocktails />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </BrowserRouter>
-
-    );
-}
+        <>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/private" element={<Private />}>
+                <Route path="/private/private-home" element={<PrivateHome />} />
+            </Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/cocktails" element={<Cocktails />} />
+        </Routes>
+        <SignUpModal />
+        <SignInModal />
+        </>
+        );
+    }
 
 export default App;
