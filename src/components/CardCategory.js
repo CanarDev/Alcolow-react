a86ec9ecfc4c26cdb69da688601318c02796a9b2
CanarDev <vincent.michel@edu.devinci.Fr>
2023-02-05 15:40:18 +0100

    :lipstick: Update cardCategory style

--- src/components/CardCategory.js
+++ src/components/CardCategory.js
@@ -5,14 +5,16 @@ function CardCategory() {
 
     return (
         <div className="
-            w-fit
+            w-52
             rounded-xl
             bg-grey
-            py-8
-            px-7
+            p-6
             space-y-4
+            flex
+            flex-col
+            items-center
         ">
-            <div className="bg-grey-darker w-36 h-36"></div>
+            <div className="bg-grey-darker w-36 h-36 rounded-md"></div>
             <p className="
                 text-center
             ">Category</p>
