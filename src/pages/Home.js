29be9695905de492138b5e32f3ca84f11e8d77b2
CanarDev <vincent.michel@edu.devinci.Fr>
2023-02-05 18:15:48 +0100

    :white_check_mark: Page home complete style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import SectionHome from "../components/SectionHome";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
+            <SectionHome isBackgroundBlack={true} />
+            <SectionHome isBackgroundBlack={false} />
+            <div className="
+                flex flex-col
+                md:grid
+                grid-cols-2
+                gap-4
+                px-4
+                py-16
+            ">
+                <SectionHome isBackgroundBlack={true} />
+                <SectionHome isBackgroundBlack={true} />
+                <SectionHome isBackgroundBlack={true} />
+                <SectionHome isBackgroundBlack={false} />
+            </div>
         </div>
     );
 }
