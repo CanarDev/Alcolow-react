5fad81b5b54c97535237b0fea9c2f97547c2152e
CanarDev <vincent.michel@edu.devinci.Fr>
2023-02-06 01:06:39 +0100

    :white_check_mark: finish home page

--- src/components/SectionHome.js
+++ src/components/SectionHome.js
@@ -1,51 +1,53 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function SectionHome(props) {
-    return (
-        <div className={`
+    if (props.cocktail) {
+        return (
+            <div className={`
             pt-12
             ${props.isBackgroundBlack ? "bg-black text-grey" : "bg-grey" }
         `}>
-            <div className="
+                <div className="
                 max-w-lg
                 mx-auto
                 px-8
             ">
-                <div className="
+                    <div className="
                     flex
                     flex-col
                     gap-4
                     mb-14
                 ">
-                    <h2 className="
+                        <h2 className="
                         text-4xl
                         text-center
                         font-bold
-                    ">Title Section</h2>
-                    <p className="
+                    ">{props.cocktail.strDrink}</h2>
+                        <p className="
                         text-3xl
                         text-center
-                    ">subhead</p>
-                    <div className="
+                    ">{props.cocktail.strCategory}</p>
+                        <div className="
                         flex justify-around
                         text-primary
                         text-xl
                     ">
-                        <p>En savoir plus ></p>
-                        <p>Acheter ></p>
+                            <Link to={`/cocktail/${props.cocktail.idDrink}`}>En savoir plus ></Link>
+                        </div>
                     </div>
-                </div>
-                <img src="" alt="" className={`
+                    <img src={props.cocktail.strDrinkThumb} alt="" className={`
                     h-80
                     w-full
                     border-0
                     object-cover
                     ${props.isBackgroundBlack ? "bg-grey-darker" : "bg-white"}
                 `}/>
+                </div>
             </div>
-        </div>
 
-    );
+        );
+    }
 }
 
 export default SectionHome;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import SectionHome from "../components/SectionHome";
+import axios from "axios";
+
 
 const Home = () => {
+    const [randCocktails, setRandCocktails] = React.useState([])
+
+    const fetchAleatoryCocktails = (end) => {
+
+        for (let i = 1; i <= end; i++) {
+            axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+                .then((rescocktailRand) => {
+                    console.log(rescocktailRand.data.drinks[0]);
+                    setRandCocktails(prevCocktail => [...prevCocktail, rescocktailRand.data.drinks[0]])
+                });
+        }
+
+
+    }
+
+    useEffect(() => {
+        fetchAleatoryCocktails(6)
+    }, [])
+
     return (
         <>
-            <SectionHome isBackgroundBlack={true} />
-            <SectionHome isBackgroundBlack={false} />
+            <SectionHome isBackgroundBlack={true} cocktail={randCocktails[0]} />
+            <SectionHome isBackgroundBlack={false} cocktail={randCocktails[1]} />
             <div className="
                 flex flex-col
                 md:grid
@@ -15,10 +36,10 @@ const Home = () => {
                 px-4
                 py-16
             ">
-                <SectionHome isBackgroundBlack={true} />
-                <SectionHome isBackgroundBlack={true} />
-                <SectionHome isBackgroundBlack={true} />
-                <SectionHome isBackgroundBlack={false} />
+                <SectionHome isBackgroundBlack={true} cocktail={randCocktails[2]} />
+                <SectionHome isBackgroundBlack={true} cocktail={randCocktails[3]} />
+                <SectionHome isBackgroundBlack={true} cocktail={randCocktails[4]} />
+                <SectionHome isBackgroundBlack={false} cocktail={randCocktails[5]} />
             </div>
         </>
     );
